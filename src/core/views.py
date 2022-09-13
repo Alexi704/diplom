2dfcd338769d6e013548a2b4d7f080db0442efe4
Tuman8 <i-a-l-e-x@yandex.ru>
2022-09-13 17:46:26 +0700

    вход на сайт: login (http://127.0.0.1:8000/core/login)

--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,20 @@
+from django.contrib.auth import login
 from rest_framework import generics
+from rest_framework.response import Response
 
-from core.serializers import CreateUserSerializer
+from core.serializers import CreateUserSerializer, LoginSerializer
 
 
 class SignupView(generics.CreateAPIView):
     serializer_class = CreateUserSerializer
+
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        login(request=request, user=user)
+        return Response(serializer.data)
