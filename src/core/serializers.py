fd6f2800abfb5ad8f13eea66d56da373a2022c3f
Tuman8 <i-a-l-e-x@yandex.ru>
2022-09-13 10:22:45 +0700

    view -> signup

---
+++ src/core/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from core.models import User
+
+
+class PasswordField(serializers.CharField):
+
+    def __int__(self, **kwargs):
+        kwargs['style'] = {'input_type', 'password'}
+        kwargs.setdefault('write_only', True)
+        super().__init__(**kwargs)
+        self.validators.append(validate_password)
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    password = PasswordField(required=True)
+    password_repeat = PasswordField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'password_repeat']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_repeat']:
+            raise ValidationError('Password mast match')
+        return attrs
+
+    def create(self, validated_data):
+        del validated_data['password_repeat']
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
--- src/todolist/urls.py
+++ src/todolist/urls.py
@@ -1,10 +1,11 @@
 """todolist URL Configuration"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from todolist.view import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ping/', health_check, name='health-check'),
+    path('core/', include('core.urls')),
 ]
