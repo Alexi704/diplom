e9fde95883b9d9bc34f827b588c324be7b6aedd3
Tuman8 <i-a-l-e-x@yandex.ru>
2022-10-11 21:28:18 +0700

    echo bot

--- src/bot/management/commands/runbot.py
+++ src/bot/management/commands/runbot.py
@@ -15,4 +15,5 @@ class Command(BaseCommand):
             res = self.tg_client.get_updates(offset=offset)
             for item in res.result:
                 offset = item.update_id + 1
-                print(item.message)
+                self.tg_client.send_message(chat_id=item.message.chat.id, text=item.message.text)  # echo bot
+                # print(item.message)
