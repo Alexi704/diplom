014e7644e44f6b446407e9a7836ac8b4ef7846fa
Tuman8 <i-a-l-e-x@yandex.ru>
2022-10-19 20:24:24 +0700

    test1

--- src/todolist/settings.py
+++ src/todolist/settings.py
@@ -100,8 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
 
-LANGUAGE_CODE = 'ru'
-# LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
